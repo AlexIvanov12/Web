9927d753866321f2fefeb3beab9c3bb7969e61f6
Oleksandr Ivanov <165718178+AlexIvanov12@users.noreply.github.com>
2024-05-25 18:35:22 -0500

    vesrion 0.1

--- server.js
+++ server.js
@@ -4,3 +4,34 @@
 // const y = [1, 2, 3]
 // console.log(y[2])
 
+
+const express = require('express')
+const app = express()
+const port = 3000
+const messages_list = []
+
+
+app.get('/message', (req, res) => 
+{
+  messages_list.push(req.query.text)
+  res.statusCode = 200
+  res.send()
+})
+
+app.get('/read', (req, res) =>
+{
+    res.send(messages_list)
+    console.log(messages_list)
+})
+
+app.get('/', function(req, res)
+{
+  res.sendFile(__dirname + '/index.html');
+  });
+
+app.listen(port, () => 
+{
+  console.log(`Example app listening on port ${port}`)
+})
+
+
